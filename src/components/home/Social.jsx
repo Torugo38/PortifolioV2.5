7afc27d934958fd49dcdc4e738e3992810b45df5
Torugo38 <victor.criscollo@sga.pucminas.br>
2024-01-30 15:29:45 -0300

    Update

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/victor-hugo-moreira-5ab559235/" className="home__social-icon" target='_blank'>
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            
+            <a href="https://github.com/Torugo38" className="home__social-icon" target='_blank'>
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    );
+};
+
+export default Social;
