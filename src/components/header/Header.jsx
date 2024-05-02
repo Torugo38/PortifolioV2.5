d426354bf10014d2384d2ad366ecf123c1a3c5fa
Torugo38 <victor.criscollo@sga.pucminas.br>
2024-05-01 23:11:53 -0300

    att

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
                         </li>
 
                         <li className="nav__item">
-                            <a href="portifolio" className="nav__link">
+                            <a href="#projetos" className="nav__link">
                                 <i className="uil uil-scenery nav__icon"></i> Projetos
                             </a>
                         </li>
