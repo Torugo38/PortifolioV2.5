f374129975cc8c92afa24246e43d33071f53c759
Torugo38 <victor.criscollo@sga.pucminas.br>
2024-05-17 13:41:52 -0300

    Mudanças

--- src/components/projetos/Projetos.jsx
+++ src/components/projetos/Projetos.jsx
@@ -8,57 +8,27 @@ import project3Img from "../../assets/project-3.png";
 
 const Projetos = () => {
   return (
-    <section id="projects">
+    <section id="projetos">
       <h2 className="section__title">Projetos</h2>
-      <span className="section__subtitle">Meus últimos projetos</span>
-      <div className="experience-details-container">
-        <div className="about-containers">
-          <div className="details-container color-container">
-            <div className="article-container">
-            <img src={project1Img} alt="Project 1" className="project-img"/>
-            </div>
-            <h2 className="experience-sub-title project-title">
-              Projeto Drunk&Drink
-            </h2>
-            <div className="btn-container">
-              <button
-                className="btn btn-color-2 project-btn"
-                onClick={() =>
-                  window.open("https://github.com/ICEI-PUC-Minas-PPLES-TI/plf-es-2022-1-ti2-0924100-drunk-drink")
-                }
-              >
-                Github
-              </button>
-            </div>
-          </div>
-          <div className="details-container color-container">
-            <div className="article-container">
-            <img src={project2Img} alt="Project 2" className="project-img"/>
-            </div>
-            <h2 className="experience-sub-title project-title">Projeto Calculadora Simples</h2>
-            <div className="btn-container">
-              <button
-                className="btn btn-color-2 project-btn"
-                onClick={() => window.open("https://github.com/Torugo38/Calculadora-Simples")}
-              >
-                Github
-              </button>
-            </div>
-          </div>
-          <div className="details-container color-container">
-            <div className="article-container">
-            <img src={project3Img} alt="Project 3" className="project-img" />
-            </div>
-            <h2 className="experience-sub-title project-title">Projeto PortfólioV1</h2>
-            <div className="btn-container">
-              <button
-                className="btn btn-color-2 project-btn"
-                onClick={() => window.open("https://github.com/Torugo38/PortifolioV1")}
-              >
-                Github
-              </button>
-            </div>
-          </div>
+      <span className="section__subtitle">Veja Meus Últimos</span>
+      <div className="projetos-container">
+        <div className="projeto-box">
+          <img src={project1Img} alt="Project 1" className="project-img" />
+          <h3 className="project-title">Drunk&Drink</h3>
+            <a href="https://github.com/ICEI-PUC-Minas-PPLES-TI/plf-es-2022-1-ti2-0924100-drunk-drink" className="projeto__button" target="_blank">Github
+                        <i className="bx bx-right-arrow-alt projeto__button-icon"></i></a>
+        </div>
+        <div className="projeto-box">
+          <img src={project2Img} alt="Project 2" className="project-img" />
+          <h3 className="project-title">Calculadora Simples</h3>
+          <a href="https://github.com/Torugo38/Calculadora-Simples" className="projeto__button" target="_blank">Github
+                        <i className="bx bx-right-arrow-alt projeto__button-icon"></i></a>
+        </div>
+        <div className="projeto-box">
+          <img src={project3Img} alt="Project 3" className="project-img" />
+          <h3 className="project-title">Portfólio</h3>
+          <a href="https://github.com/Torugo38/PortifolioV2.5" className="projeto__button" target="_blank">Github
+                        <i className="bx bx-right-arrow-alt projeto__button-icon"></i></a>
         </div>
       </div>
     </section>
