ad043d93c969a9b06981c6e392c4a3b28f5c04b2
Torugo38 <victor.criscollo@sga.pucminas.br>
2024-05-24 05:47:35 -0300

    att

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -14,14 +14,14 @@ const Info = () => {
         <i class='bx bx-briefcase about__icon' ></i>
 
         <h3 className="about__title">Experiência</h3>
-        <span className="about__subtitle">Suporte</span>
+        <span className="about__subtitle">Desenvolvimento</span>
       </div>
 
       <div className="about__box">
         <i class='bx bx-briefcase about__icon' ></i>
 
         <h3 className="about__title">Experiência</h3>
-        <span className="about__subtitle">Desenvolvimento</span>
+        <span className="about__subtitle">Suporte</span>
       </div>
     </div>
 
